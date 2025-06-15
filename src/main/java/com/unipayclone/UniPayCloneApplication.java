ca51bf64e818ae46f04f8ec1bf6b0119f731cd36
ABHI9960DEO <abhishek.virgo0001@gmail.com>
2025-06-15 15:36:33 +0530

    Initial commit

---
+++ src/main/java/com/unipayclone/UniPayCloneApplication.java
@@ -0,0 +1,13 @@
+package com.unipayclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPayCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniPayCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipayclone/UniPayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipayclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniPayCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
